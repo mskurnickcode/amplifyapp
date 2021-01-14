846f14fa97dcb98bed41eca49a31c690391b3549
mskurnickcode <mskurnickcode@gmail.com>
2021-01-14 23:06:41 +0200

    changes for v3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1> Helll from V2 </h1>
+        <h1> Hola de tu Gringo </h1>
+        <h1> Te amo </h1>
       </header>
     </div>
   );
